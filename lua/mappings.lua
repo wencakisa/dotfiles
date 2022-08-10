49bcc880ac4ae76cf5df2210cb504b82f0a83e6f
wencakisa <wencakisa@gmail.com>
2022-08-10 16:46:39 +0300

    Lua remaps enhancements

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,43 +1,65 @@
+function map(mode, shortcut, command, opts)
+  local options = { noremap = true }
+
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+
+  vim.api.nvim_set_keymap(mode, shortcut, command, options)
+end
+
+function nmap(shortcut, command, opts)
+  map('n', shortcut, command, opts)
+end
+
+function imap(shortcut, command, opts)
+  map('i', shortcut, command)
+end
+
+function vmap(shortcut, command, opts)
+  map('v', shortcut, command, opts)
+end
+
 -- Leader is <Space>
 vim.g.mapleader = ' '
 
 -- Comment out lines with Ctrl + /
-vim.api.nvim_set_keymap('n', '<C-_>', '<esc>:Commentary<cr>', { noremap = true })
-vim.api.nvim_set_keymap('i', '<C-_>', '<esc>:Commentary<cr>', { noremap = true })
-vim.api.nvim_set_keymap('v', '<C-_>', ':Commentary<cr>', { noremap = true })
+nmap('<C-_>', '<esc>:Commentary<cr>')
+imap('<C-_>', '<esc>:Commentary<cr>')
+vmap('<C-_>', ':Commentary<cr>')
 
 -- gd to Go to definition (in the same tab)
-vim.api.nvim_set_keymap('n', 'gd', '<Plug>(coc-definition)', { silent = true })
+nmap('gd', '<Plug>(coc-definition)', { silent = true })
 -- gD to Go to definition (in a new tab)
-vim.api.nvim_set_keymap('n', 'gD', ":call CocAction('jumpDefinition', 'tab drop')<cr>", { silent = true })
+nmap('gD', ":call CocAction('jumpDefinition', 'tab drop')<cr>", { silent = true })
 
 -- Ctrl+p to search files with FZF
-vim.api.nvim_set_keymap('n', '<C-p>', ':Files<cr>', { noremap = true })
+nmap('<C-p>', ':Files<cr>')
 
 -- <leader>f searches the word under the cursor
-vim.api.nvim_set_keymap('n', '<leader>F', '<Plug>CtrlSFPrompt', {})
-vim.api.nvim_set_keymap('n', '<leader>f', '<Plug>CtrlSFCwordExec', {})
-vim.api.nvim_set_keymap('v', '<leader>f', '<Plug>CtrlSFVwordExec', {})
+nmap('<leader>F', '<Plug>CtrlSFPrompt')
+nmap('<leader>f', '<Plug>CtrlSFCwordExec')
+vmap('<leader>f', '<Plug>CtrlSFVwordExec')
 
 -- Go to tab by number
-vim.api.nvim_set_keymap('n', '<leader>1', '1gt', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>2', '2gt', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>3', '3gt', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>4', '4gt', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>5', '5gt', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>6', '6gt', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>7', '7gt', { noremap = true })
+nmap('<leader>1', '1gt')
+nmap('<leader>2', '2gt')
+nmap('<leader>3', '3gt')
+nmap('<leader>4', '4gt')
+nmap('<leader>5', '5gt')
+nmap('<leader>6', '6gt')
+nmap('<leader>7', '7gt')
 
 -- Join lines without redundant <space>
-vim.api.nvim_set_keymap('', 'J', 'gJ', { noremap = true })
+map('', 'J', 'gJ')
 
 -- Scroll COC suggestion list with tab
-vim.api.nvim_set_keymap('i', '<Tab>', 'pumvisible() ? "<C-n>" : "<Tab>"', { noremap = true, expr = true })
-vim.api.nvim_set_keymap('i', '<S-Tab>', 'pumvisible() ? "<C-p>" : "<S-Tab>"', { noremap = true, expr = true })
+imap('<Tab>', 'pumvisible() ? "<C-n>" : "<Tab>"', { expr = true })
+imap('<S-Tab>', 'pumvisible() ? "<C-p>" : "<S-Tab>"', { expr = true })
 
 -- NERDTree bindings
-vim.api.nvim_set_keymap('n', '<C-t>', ':NERDTreeToggle<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-f>', ':NERDTreeFind<cr>', { noremap = true })
+nmap('<C-t>', ':NERDTreeToggle<cr>')
+nmap('<C-f>', ':NERDTreeFind<cr>')
 
 -- .vimrc binding
-vim.api.nvim_set_keymap('n', '<leader>ev', ':tabnew ~/.vimrc<cr>', { noremap = true })
+nmap('<leader>ev', ':tabnew ~/.vimrc<cr>')
